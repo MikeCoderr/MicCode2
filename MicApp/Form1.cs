e9140362a9deeef3e89641fb21ca020cb44a06c8
bugrpators-yashu <singhyashpal@seasiainfotech.com>
2016-08-24 14:15:12 +0530

    Changing

--- MicApp/Form1.cs
+++ MicApp/Form1.cs
@@ -27,6 +27,7 @@ namespace MicApp
         static IWebElement finalElement = null;
         static WebDriverWait wait = null;
         static Int16 loopCount = 0;
+        static string msg = "";
 
         //Reading values from app.config file
 
@@ -37,6 +38,7 @@ namespace MicApp
         {
             InitializeComponent();
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -56,7 +58,7 @@ namespace MicApp
                     loginFuntion(); // Common funtion for login
                     createTemplate();
                 }
-                else if (isSessionExpire())
+                else if (!isSessionExpire())
                 {
                     loginFuntion(); // Common funtion for login
                     createTemplate();
@@ -71,6 +73,7 @@ namespace MicApp
                 MessageBox.Show("There was some error." + ex);
             }
         }
+
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -101,139 +104,147 @@ namespace MicApp
         {
             driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(30));
             Thread.Sleep(15000);
-
+            String templates = "";
+            int recordnumber = 1;
             Actions a = new Actions(driver);
-            for (int i = 0; i <= 100; i++)
+            try
             {
-                try
+                for (int i = 0; i <= 100; i++)
                 {
-                    driver.SwitchTo().DefaultContent();
-                    driver.SwitchTo().Frame(0);
-
-                    finalElement = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("manageClients"))));
-
-                    if (finalElement != null)
+                    try
                     {
-                        Thread.Sleep(2000);
+                        driver.SwitchTo().DefaultContent();
+                        driver.SwitchTo().Frame(0);
+
+                        finalElement = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("manageClients"))));
+
+                        if (finalElement != null)
+                        {
+                            Thread.Sleep(2000);
+                            a.MoveToElement(driver.FindElement(By.XPath("//*[@id='manageClients']/a"))).Perform();
+                            Thread.Sleep(2000);
+                            break;
+
+                        }
+                        else
+                        {
+                            Thread.Sleep(2000);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Thread.Sleep(1000);
+                        finalElement = null;
+                    }
+
+                }
+
+                for (int i = 0; i <= 100; i++)
+                {
+                    try
+                    {
                         a.MoveToElement(driver.FindElement(By.XPath("//*[@id='manageClients']/a"))).Perform();
                         Thread.Sleep(2000);
-                        break;
+                        finalElement = driver.FindElement(By.XPath("//*[@id='clientFees']/a"));
+                        if (finalElement != null)
+                        {
+                            Thread.Sleep(2000);
+                            element = driver.FindElement(By.Id("clientFees"));
+                            element.FindElements(By.TagName("a"))[0].Click();
+                            Thread.Sleep(2000);
+                            element.FindElements(By.TagName("a"))[0].Click();
+                            Thread.Sleep(2000);
+                            element.FindElements(By.TagName("a"))[0].Click();
 
+                            break;
+                        }
+                        else
+                        {
+                            Thread.Sleep(1000);
+                        }
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        Thread.Sleep(2000);
+
+                        Thread.Sleep(1000);
+                        finalElement = null;
+                    }
+
+                }
+
+                for (int i = 0; i <= 100; i++)
+                {
+                    try
+                    {
+
+                        finalElement = driver.FindElement(By.Id("feeTemplate"));
+                        if (finalElement != null)
+                        {
+                            element = driver.FindElement(By.Id("feeTemplate"));
+                            element.FindElements(By.TagName("a"))[0].Click();
+                            break;
+                        }
+                        else
+                        {
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Thread.Sleep(1000);
+                        finalElement = null;
                     }
                 }
-                catch (Exception ex)
-                {
+                driver.SwitchTo().DefaultContent();
+                Int16 x = Convert.ToInt16(driver.FindElements(By.TagName("frame")).Count());
 
-                    Thread.Sleep(1000);
-                    finalElement = null;
+                IList<IWebElement> elements = driver.FindElements(By.TagName("frame"));
+
+                for (int i = 0; i <= x; i++)
+                {
+                    String value = elements[i].GetAttribute("id");
+                    if (value == "content")
+                    {
+                        driver.SwitchTo().Frame(i);
+                        String name = driver.FindElement(By.Id("newTemplate")).GetAttribute("title");
+                        break;
+                    }
+
                 }
 
-            }
 
-            for (int i = 0; i <= 100; i++)
-            {
-                try
+               
+                for (recordnumber = 1; recordnumber <= loopCount; recordnumber++)
                 {
-                    a.MoveToElement(driver.FindElement(By.XPath("//*[@id='manageClients']/a"))).Perform();
+                    Thread.Sleep(5000);
+                    wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("newTemplate")))).Click();
+                    String st = GenerateRandomAlphaNumericCode(10);
+                    Thread.Sleep(1000);
+                    //file template
+                    wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Name("templateName")))).SendKeys(st);
+                    //expand stock
                     Thread.Sleep(2000);
-                    finalElement = driver.FindElement(By.XPath("//*[@id='clientFees']/a"));
-                    if (finalElement != null)
-                    {
-                        Thread.Sleep(2000);
-                        element = driver.FindElement(By.Id("clientFees"));
-                        element.FindElements(By.TagName("a"))[0].Click();
-                        Thread.Sleep(2000);
-                        element.FindElements(By.TagName("a"))[0].Click();
-                        Thread.Sleep(2000);
-                        element.FindElements(By.TagName("a"))[0].Click();
-
-                        break;
-                    }
-                    else
-                    {
-                        Thread.Sleep(1000);
-                    }
+                    wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("pspan_STK")))).Click();
+                    Thread.Sleep(2000);
+                    //enter data
+                    driver.FindElement(By.Name("USD/STK_TICKET_CHARGE")).SendKeys("1");
+                    driver.FindElement(By.Name("USD/STK_MU_ABS")).SendKeys("0.07");
+                    //click on continue id
+                    Thread.Sleep(2000);
+                    wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("continueID")))).Click();
+                    Thread.Sleep(7000);
+                    wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("continueID")))).Click();
+                    templates = templates + "," + st;
                 }
-                catch (Exception ex)
-                {
-
-                    Thread.Sleep(1000);
-                    finalElement = null;
-                }
-
+                msg = "Total template created:(" + loopCount + ")" + Environment.NewLine + "Following template were created:" + Environment.NewLine + templates;
+                MessageBox.Show("Total template created:" + loopCount + "Names were:", Environment.NewLine + templates);
             }
-
-            for (int i = 0; i <= 100; i++)
+            catch (Exception ex)
             {
-                try
-                {
-
-                    finalElement = driver.FindElement(By.Id("feeTemplate"));
-                    if (finalElement != null)
-                    {
-                        element = driver.FindElement(By.Id("feeTemplate"));
-                        element.FindElements(By.TagName("a"))[0].Click();
-                        break;
-                    }
-                    else
-                    {
-                        Thread.Sleep(1000);
-                    }
-                }
-                catch (Exception ex)
-                {
-
-                    Thread.Sleep(1000);
-                    finalElement = null;
-                }
+                MessageBox.Show("Error occurred while attempting("+ recordnumber + ") record.");
             }
-            driver.SwitchTo().DefaultContent();
-            Int16 x = Convert.ToInt16(driver.FindElements(By.TagName("frame")).Count());
-
-            IList<IWebElement> elements = driver.FindElements(By.TagName("frame"));
-
-            for (int i = 0; i <= x; i++)
-            {
-                String value = elements[i].GetAttribute("id");
-                if (value == "content")
-                {
-                    driver.SwitchTo().Frame(i);
-                    String name = driver.FindElement(By.Id("newTemplate")).GetAttribute("title");
-                    break;
-                }
-
-            }
-
-
-            String templates = "";
-            for (int i = 1; i <= loopCount; i++)
-            {
-                Thread.Sleep(3000);
-                wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("newTemplate")))).Click();
-                String st = GenerateRandomAlphaNumericCode(10);
-                Thread.Sleep(1000);
-                //file template
-                wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Name("templateName")))).SendKeys(st);
-                //expand stock
-                Thread.Sleep(2000);
-                wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("pspan_STK")))).Click();
-                Thread.Sleep(2000);
-                //enter data
-                driver.FindElement(By.Name("USD/STK_TICKET_CHARGE")).SendKeys("1");
-                driver.FindElement(By.Name("USD/STK_MU_ABS")).SendKeys("0.07");
-                //click on continue id
-                Thread.Sleep(2000);
-                wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("continueID")))).Click();
-                Thread.Sleep(7000);
-                wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("continueID")))).Click();
-                templates = templates + "," + st;
-            }
-
-            MessageBox.Show("Total template created:" + loopCount + "Names were:" + templates);
         }
 
         //Create Funding main funtion
@@ -243,10 +254,12 @@ namespace MicApp
             {
                 createDriver(); // Common funtion for driver
                 loginFuntion(); // Common funtion for login
+                createFunding(); // Creating fund
             }
             else if (isSessionExpire())
             {
                 loginFuntion(); // Common funtion for login
+                createFunding();
             }
             else
             {
@@ -265,35 +278,51 @@ namespace MicApp
         //Creating funding called by Funding main funtion
         public static void createFunding()
         {
-            
-            driver.SwitchTo().DefaultContent();
-            Thread.Sleep(1000);
-            driver.SwitchTo().Frame(0);
-            Thread.Sleep(1000);
-            wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("funding")))).Click();
-            Thread.Sleep(10000);
-            element = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("fundTrans"))));
-            element.FindElements(By.TagName("a"))[0].Click();
-            Thread.Sleep(10000);
-            driver.SwitchTo().DefaultContent();
-            Thread.Sleep(1000);
-            driver.SwitchTo().Frame(2);
-            SelectElement tranType = new SelectElement(driver.FindElement(By.Id("transaction")));
-            tranType.SelectByText("Internal Transfers");
-            Thread.Sleep(10000);
-            SelectElement accNum = new SelectElement(driver.FindElement(By.Id("fromAcct")));
-            accNum.SelectByText("I1641010");
-            Thread.Sleep(10000);
-            element = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("amountRow"))));
-            element.FindElements(By.TagName("input"))[0].SendKeys("0.01");
-            Thread.Sleep(10000);
-            SelectElement currency = new SelectElement(driver.FindElement(By.Id("currency")));
-            currency.SelectByText("RUB");
-            Thread.Sleep(10000);
-            SelectElement destAccNum = new SelectElement(driver.FindElement(By.Id("currency")));
-            currency.SelectByText("F1845979");
-            Thread.Sleep(10000);
+            try
+            {
+                IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+                Actions a = new Actions(driver);
+                driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(30));
+                Thread.Sleep(15000);
+                driver.SwitchTo().DefaultContent();
+                Thread.Sleep(1000);
+                driver.SwitchTo().Frame(0);
+                Thread.Sleep(2000);
 
+                a.MoveToElement(wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("funding"))))).Perform();
+                Thread.Sleep(2000);
+                element = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("fundTrans"))));
+                element.FindElements(By.TagName("a"))[0].Click();
+                Thread.Sleep(500);
+                //js.ExecuteScript("arguments[0].click();", element);
+                element.FindElements(By.TagName("a"))[0].Click();
+                Thread.Sleep(10000);
+                driver.SwitchTo().DefaultContent();
+                Thread.Sleep(1000);
+                driver.SwitchTo().Frame(2);
+                Thread.Sleep(2000);
+                SelectElement tranType = new SelectElement(driver.FindElement(By.Id("transaction")));
+                tranType.SelectByText("Internal Transfers");
+                Thread.Sleep(10000);
+                SelectElement accNum = new SelectElement(wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("fromAcct")))));
+                accNum.SelectByText("I1641010");
+                Thread.Sleep(2000);
+                element = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("amountRow"))));
+                element.FindElements(By.TagName("input"))[0].SendKeys("0.01");
+                Thread.Sleep(2000);
+                SelectElement currency = new SelectElement(driver.FindElement(By.Id("currency")));
+                currency.SelectByText("RUB");
+                Thread.Sleep(2000);
+                SelectElement destAccNum = new SelectElement(driver.FindElement(By.Id("to_acct_id")));
+                destAccNum.SelectByText("F1845979");
+                Thread.Sleep(10000);
+          
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There is some error occured:-" + ex);
+            }
 
         }
 
@@ -312,23 +341,32 @@ namespace MicApp
         //Verifying if session was expired
         public static Boolean isSessionExpire()
         {
-
-
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
-            driver.SwitchTo().DefaultContent();
-            Thread.Sleep(1000);
-            driver.SwitchTo().Frame(0);
-            Thread.Sleep(1000);
-            wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("home")))).Click();
-            Thread.Sleep(10000);
-            finalElement = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("manageClients"))));
-            if (finalElement == null)
+            try
             {
-                return false;
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+                driver.SwitchTo().DefaultContent();
+                Thread.Sleep(1000);
+                driver.SwitchTo().Frame(0);
+                Thread.Sleep(1000);
+                wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("home")))).Click();
+                Thread.Sleep(10000);
+                finalElement = wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.XPath("//*[@id='contents']/div/table/tbody/tr[1]/td"))));
+                String x = finalElement.Text;
+                if (x == "")
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return true;
+                if (ex.ToString().Contains("such"))
+                    return false;
+                else
+                    return false;
             }
         }
 
